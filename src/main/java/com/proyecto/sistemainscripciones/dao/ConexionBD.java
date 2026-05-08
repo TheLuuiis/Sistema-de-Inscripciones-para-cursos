62305f7a128065c3a5c2017f7f1e54ce60f91257
TheLuis <luisdavidgilcabarcas47@gmail.com>
2026-05-07 19:30:18 -0500

    Cambio en la conexión a la BD

--- src/main/java/com/proyecto/sistemainscripciones/dao/ConexionBD.java
+++ src/main/java/com/proyecto/sistemainscripciones/dao/ConexionBD.java
@@ -8,7 +8,7 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/sistema_inscripciones?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "admin";
 
     public static Connection conectar() throws SQLException {
         try {
